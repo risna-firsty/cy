c59f95a975fadf4045c7ca618e14b37babc4d857
Risna Setiyana <risnasetiyana@Risnas-MacBook-Pro.local>
2023-09-08 19:36:26 +0700

    edit script to get login session

--- cypress/e2e/privynow/2-login.cy.js
+++ cypress/e2e/privynow/2-login.cy.js
@@ -3,10 +3,10 @@
 describe("Test Login Page of PrivyNow", () => {
     beforeEach(() => {
       cy.viewport(1200, 800)
+      cy.visit('https://app.privynow.com/');
     });
   
     it("Should be able to access login page", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('h1').should('contain', 'Log in')
       cy.get(".text-center").should('contain', 'Or, log in using')
       cy.url().should('include', 'sign-in')
@@ -14,13 +14,11 @@ describe("Test Login Page of PrivyNow", () => {
   
   
     it("Should not allowed to input invalid email format", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('input[data-cy="email"]').type('risrisgmail.com')
       cy.get(".sm").should('contain', 'Please enter a valid email address.') //kalimat validasinya apa
     })
   
     it("Should enable login button after fill in both field with acceptable data", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('input[data-cy="email"]').type('risnaprod1@mailinator.com')
       cy.get('input[data-cy="password"]').type('lalala')
       cy.get('button[data-cy="submit"]').should('have.class', 'btn-primary')
@@ -28,20 +26,17 @@ describe("Test Login Page of PrivyNow", () => {
   
   
     it("Should not be able to log in with valid email only", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('input[data-cy="email"]').type('risnaprod1@mailinator.com')
       cy.get('button[data-cy="submit"]').should('have.class', 'btn-disabled')
     })
   
   
     it("Should not be able to log in with password only", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('input[data-cy="password"]').type('lalala')
       cy.get('button[data-cy="submit"]').should('have.class', 'btn-disabled')
     })
   
     it("Should be able to log in with valid data", () => {
-      cy.visit('https://app.privynow.com/');
       cy.get('input[data-cy="email"]').type('risnaprod1@mailinator.com')
       cy.get('input[data-cy="password"]').type('Qwert123')
       cy.get('button[data-cy="submit"]').should('have.class', 'btn-primary')
